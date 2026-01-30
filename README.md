8d10e2fc47b6311ca110f8ea129facc6609da4cb
vinisilvaoliveira <viniciussoliveira8@gmail.com>
2026-01-29 21:23:42 -0300

    Feat: componente de banner

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # curso-react-alura-luz-e-cena
+
+1 -Clonar repositorio 
+2 - npm run dev
+
+Para criar um componente e criar a estrutura react: rafce
---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import Banner from "./Banner";
+
+function App() {
+
+  return <Banner src="./Imagens/Banner-principal/banner.png" alt="Banner"/>
+}
+
+export default App;
