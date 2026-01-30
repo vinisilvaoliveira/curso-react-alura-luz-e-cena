dc6a5ac700f5eb7ee7fcde54096ead2e151cf01b
vinisilvaoliveira <viniciussoliveira8@gmail.com>
2026-01-29 21:41:38 -0300

    Incluindo componente de Link

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import Banner from "./Banner";
+import Link from "./link";
 
 function App() {
 
-  return <Banner src="./Imagens/Banner-principal/banner.png" alt="Banner"/>
+  return (
+    <>
+     <Banner src="./Imagens/Banner-principal/banner.png" alt="Banner"/>
+     <Link href="/" target="_blank">Link</Link>
+    </>
+  )
 }
 
 export default App;
