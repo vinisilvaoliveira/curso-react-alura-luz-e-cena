7dd769460f24d411a0b5930c86a964b0bc82f9b2
vinisilvaoliveira <viniciussoliveira8@gmail.com>
2026-02-03 22:29:52 -0300

    feat: inclusao sessão lista de filmes

---
+++ src/components/CardMovie/index.tsx
@@ -0,0 +1,27 @@
+import styles from "./CardMovie.module.css";
+import Tag from "../Tag";
+import { type Movie } from "../../types";
+
+const CardMovie = (props: Movie) => {
+  const { alt, src, titulo, genero, categoria, censura, duracao } = props;
+  return (
+    <li className={styles.card}>
+      <img src={src} alt={alt} />
+      <div className={styles.container}>
+        <h3>{titulo}</h3>
+        <div className={styles.informacoes}>
+          <div className={styles.linha1}>
+            <p>{genero}</p>
+            <Tag value={categoria} />
+          </div>
+          <div className={styles.linha2}>
+            <p>{duracao}</p>
+            <Tag value={censura} />
+          </div>
+        </div>
+      </div>
+    </li>
+  );
+};
+
+export default CardMovie;
---
+++ src/components/MovieList/index.tsx
@@ -0,0 +1,19 @@
+import styles from "./MovieList.module.css";
+import CardMovie from "../CardMovie";
+import { type Movie } from "../../types";
+
+interface MovieListProps {
+  movies: Movie[];
+}
+
+const MovieList = ({ movies }: MovieListProps) => {
+  return (
+    <ul className={styles.lista}>
+      {movies.map((movie) => (
+        <CardMovie key={movie.id} {...movie} />
+      ))}
+    </ul>
+  );
+};
+
+export default MovieList;
